b58245dd2cf6b3100602bb8709d2610b5c6130bb
Kevin Bohinski <kbohinski@gmail.com>
2017-04-22 20:25:07 -0400

    cleaning some things

--- static/js/onboard.js
+++ static/js/onboard.js
@@ -1,13 +1,19 @@
 var dropdownTitle = document.getElementById('dropdownMenuButton');
 var items = document.getElementsByClassName('dropdown-item');
 
+var n = document.getElementById('n');
+var numbers = document.getElementById('numbers');
+
+var nHtml = n.innerHTML;
+var numbersHtml = numbers.innerHTML;
+
 function showN() {
-    n.innerHTML = '<label class="control-label" for="n">n</label><div class="controls"><input id="n" name="n" placeholder="20" class="form-control input-md" required="" type="text"></div>';
+    n.innerHTML = nHtml;
     n.style.visibility = 'visible';
 }
 
 function showNumbers() {
-    numbers.innerHTML = '<label class="control-label" for="numbers">numbers (enter seperated by comma)</label><div class="controls"><input id="numbers" name="numbers" placeholder="ex: \'1234567890,6094753287\'" class="form-control input-md" required="" type="textarea"></div>';
+    numbers.innerHTML = numbersHtml;
     numbers.style.visibility = 'visible';
 }
 
@@ -20,9 +26,6 @@ function hideNumbers() {
     numbers.innerHTML = '';
     numbers.style.visibility = 'hidden';
 }
-
-var n = document.getElementById('n');
-var numbers = document.getElementById('numbers');
 
 hideN();
 hideNumbers();
